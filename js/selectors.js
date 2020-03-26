f8f4063d9e12c91a1771fa7865ff9d0257e86620
Oscar <mundonatural@gmail.com>
2020-03-26 11:08:44 +0000

    Automatically backed up by Learn

--- js/selectors.js
+++ js/selectors.js
@@ -6,5 +6,9 @@ function paragraphSelector() {
 }
 
 function lastImageSelector() {
-  return $('img:last')
+  return $('img:last');
+}
+
+function ninjaBabySelector() {
+  return $('#baby-ninja');
 }
